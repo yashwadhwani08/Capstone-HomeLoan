b3bc2028653a884d0a10a7727e93565f03639ace
Yash Wadhwani <yashwadhwani08@gmail.com>
2022-08-23 10:34:46 +0530

    First Commit

---
+++ homeLoan-first-app/homeLoan-first-app/src/main/java/com/homeLoan/homeLoanfirstapp/HomeLoanFirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.homeLoan.homeLoanfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLoanFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeLoanFirstAppApplication.class, args);
+	}
+
+}
---
+++ homeLoan-first-app/homeLoan-first-app/src/test/java/com/homeLoan/homeLoanfirstapp/HomeLoanFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeLoan.homeLoanfirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeLoanFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
